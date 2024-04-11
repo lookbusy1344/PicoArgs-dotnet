0300e50b852c79ac328ffa5c76bdab7773af439e
lookbusy1344 <3680611+lookbusy1344@users.noreply.github.com>
2024-04-11 10:05:58 +0100

    Prepping the win32 api call to split params
    (cherry picked from commit 3d32c0a53d1012079cc14582ac9d9812ad12fcc4)

--- TestPicoArgs/SplitArgs.cs
+++ TestPicoArgs/SplitArgs.cs
@@ -1,4 +1,6 @@
-﻿using PicoArgs_dotnet;
+﻿// #define WINDOWS_CALLS
+
+using PicoArgs_dotnet;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -22,13 +24,43 @@ internal static class SplitArgs
 	/// <summary>
 	/// Split a command line into arguments (adds "echo" to the front to handle the case where the first argument is quoted)
 	/// </summary>
-	internal static IEnumerable<string> SplitArgumentsLine(string line) => CommandLineToArgvW($"echo {line}").Skip(1);
+	internal static IEnumerable<string> SplitArgumentsLine(string line) => CommandLineToArgvW2($"echo {line}").Skip(1);
 
+#if WINDOWS_CALLS
+	/**
+	 * Windows native CommandLineToArgvW
+	 * Copied from https://stackoverflow.com/questions/298830/split-string-containing-command-line-parameters-into-string-in-c-sharp#answer-749653
+	 */
+	[DllImport("shell32.dll", SetLastError = true)]
+	private static extern IntPtr CommandLineToArgvW([MarshalAs(UnmanagedType.LPWStr)] string lpCmdLine, out int pNumArgs);
+
+	public static string[] CommandLineToArgvW2(string commandLine)
+	{
+		int argc;
+		var argv = CommandLineToArgvW(commandLine, out argc);
+		if (argv == IntPtr.Zero)
+			throw new System.ComponentModel.Win32Exception();
+		try
+		{
+			var args = new string[argc];
+			for (var i = 0; i < argc; i++)
+			{
+				var p = Marshal.ReadIntPtr(argv, i * IntPtr.Size);
+				args[i] = Marshal.PtrToStringUni(p)!;
+			}
+			return args;
+		}
+		finally
+		{
+			Marshal.FreeHGlobal(argv);
+		}
+	}
+#else
 	/**
 	 * C# equivalent of CommandLineToArgvW
 	 * Translated from https://source.winehq.org/git/wine.git/blob/HEAD:/dlls/shcore/main.c#l264
 	 */
-	private static string[] CommandLineToArgvW(string cmdline)
+	private static string[] CommandLineToArgvW2(string cmdline)
 	{
 		if (string.IsNullOrWhiteSpace(cmdline)) return [];
 
@@ -212,34 +244,5 @@ internal static class SplitArgs
 		}
 		return argv;
 	}
-
-	/**
-	 * Windows native CommandLineToArgvW
-	 * Copied from https://stackoverflow.com/questions/298830/split-string-containing-command-line-parameters-into-string-in-c-sharp#answer-749653
-	 */
-	/*
-	[DllImport("shell32.dll", SetLastError = true)]
-	private static extern IntPtr CommandLineToArgvW([MarshalAs(UnmanagedType.LPWStr)] string lpCmdLine, out int pNumArgs);
-
-	public static string[] CommandLineToArgvW(string commandLine)
-	{
-		int argc;
-		var argv = CommandLineToArgvW(commandLine, out argc);
-		if (argv == IntPtr.Zero)
-			throw new System.ComponentModel.Win32Exception();
-		try
-		{
-			var args = new string[argc];
-			for (var i = 0; i < argc; i++)
-			{
-				var p = Marshal.ReadIntPtr(argv, i * IntPtr.Size);
-				args[i] = Marshal.PtrToStringUni(p);
-			}
-			return args;
-		}
-		finally
-		{
-			Marshal.FreeHGlobal(argv);
-		}
-	} */
+#endif
 }
